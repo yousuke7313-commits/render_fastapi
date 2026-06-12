be46044797ab2331ecf9802846db7b2624ca4dcc
yousuke7313-commits <yousuke7313@gmail.com>
2026-06-12 12:23:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from fastapi.responses import HTMLResponse
+
 import random  # randomライブラリを追加
 
 app = FastAPI()
@@ -31,3 +33,19 @@ def omikuji():
     ]
 
     return omikuji_list[random.randrange(10)]
+
+### コードいろいろ... ###
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
